801c1565a0290f987561da3413cf308082245977
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-10-10 11:42:22 +0600

    complete enums

---
+++ ts/Enums.ts
@@ -0,0 +1,8 @@
+enum WeatherCondtions {
+  Sunny = "Sunny",
+  Cloudy = "Cloudy",
+  Rainy = "Rainy",
+  Snowy = "Snowy",
+}
+const currentWeather = WeatherCondtions.Cloudy;
+console.log(currentWeather);
