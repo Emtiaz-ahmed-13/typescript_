ff8378498e38517da7e06ee3bd2e7bd31b13c9af
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-10-10 11:37:20 +0600

    finish tuples

--- ts/Literal.ts
+++ ts/Literal.ts
@@ -1,11 +1,9 @@
 let color: "red" | "blue" | "green";
 color = "red";
-color = "yellow";
 
 let number: 1 | 2 | 3 | 4 | 5;
 number = 1;
 number = 2;
-
 let isTrue = true;
 isTrue = false;
 isTrue = true;
---
+++ ts/tuples.ts
@@ -0,0 +1,4 @@
+let myTuple: [string, number] = ["emtiaz", 25];
+
+console.log(myTuple[0]);
+console.log(myTuple[1]);
